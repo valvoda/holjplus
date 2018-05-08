a1c6583e47ffbfcf4610fad6fbf5886827932b64
valvoda <pepa.valvoda@gmail.com>
2018-05-08 12:39:27 +0900

    simplified code

--- extract.py
+++ extract.py
@@ -39,7 +39,7 @@ class Extract:
 
         return hol
 
-    def extract_body(self, htmlfile):
+    def extract_body(self, htmlfile, special):
         """
         Extracts the body of a judgement.
         """
@@ -49,8 +49,6 @@ class Extract:
             print("ERROR: could not get html!")
             return None, None
 
-        # print(html.prettify()) #visualise the html
-
         end = html.find("a", string="continue")
         if end == None:
             end = html.find("a", string="Continue")
@@ -59,36 +57,16 @@ class Extract:
             end = html.find("img", alt="continue")
             if end != None:
                 end = "continue"
+
         elif end != None:
             end = end.getText()
 
-        text = html.find_all('tbody')
-        text = [t.text for t in text]
-
-        text = text[1:-1] # removes the webpage border around the body of the case
-
-        return text, end
-
-    def extract_body_0607(self, htmlfile):
-        """
-        Extracts the body of a judgement in cases from 2006 to 2007
-        """
-        if htmlfile != None: #Check if valid file
-            html = bs(htmlfile, "html5lib") # NOTE the standard html.parser does not work here!!!
+        if special:
+            text = html.find_all('p')
         else:
-            print("ERROR: could not get html!")
-            return None, None
+            text = html.find_all('tbody')
 
-        end = html.find("a", string="continue")
-        if end == None:
-            end = html.find("a", string="Continue")
-
-        if end != None:
-            end = end.getText()
-
-        text = html.find_all('p')
         text = [t.text for t in text]
-
         text = text[1:-1] # removes the webpage border around the body of the case
 
         return text, end
@@ -97,10 +75,19 @@ class Extract:
         """
         Finds the link to the next part of a case.
         """
-        num = re.search("(\d).htm", html) #find the number # NOTE what about 01, 02...10 notation?
-        num = int(num.group(1)) + 1 #increse it by one
-        html = re.sub("(\d).htm", str(num) + ".htm", html) #pass it back in
+        num = re.search("(\d+).htm", html)
+        num = num.group(1)
 
+        if num.startswith("0"): #for 01, 02, 03... link format
+            num = int(num) + 1 #moves to next link
+            if num > 9: #if single digit
+                html = re.sub("(\d+).htm", str(num) + ".htm", html)
+            else: #if double digit
+                html = re.sub("(\d+).htm", "0" + str(num) + ".htm", html)
+
+        else:
+            num = int(num) + 1 #moves to next link
+            html = re.sub("(\d+).htm", str(num) + ".htm", html) #pass it back in
         return html
 
     def extract_case(self, html, all, year):
@@ -111,10 +98,10 @@ class Extract:
         sc = scrape.Scrape()
         raw_html = sc.simple_get(html)
 
-        if year == "07" or year == "06":
-            text, end = self.extract_body_0607(raw_html)
+        if year == "07" or year == "06": #06 and 07 formating requires special processing
+            text, end = self.extract_body(raw_html, True)
         else:
-            text, end = self.extract_body(raw_html)
+            text, end = self.extract_body(raw_html, False)
 
         if text == None:
             return False
@@ -134,45 +121,38 @@ if __name__ == "__main__":
 
     print("Testing Extract:")
 
-    # # Testing extraction of links
-    # raw_html = sc.simple_get("https://publications.parliament.uk/pa/ld/ldjudgmt.htm")
-    # links = ex.extract_links(raw_html)
-    # assert (len(links) == 835), error1
-    # assert (links[-1] == "http://www.parliament.uk/site-information/copyright/"), error2
-    #
-    # # Testing filtering of links
-    # hol = ex.filter_holinks(links)
-    # assert (len(hol) == 788), error1
-    # assert (hol[0] == "https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/assom.htm"), error2
-    # assert (hol[-1] == "https://publications.parliament.uk/pa/ld199697/ldjudgmt/jd961121/smith01.htm"), error2
-    #
-    # assert ex.next_html("https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-1.htm") \
-    #  == "https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-2.htm", "ERROR, HTML does not increase."
-    #
-    # print("Working")
-    #
-    # # Testing case extraction
-    # print("Visual inspection, check for duplicates:") #NOTE better test required
-    # all = []
-    # ex.extract_case("https://publications.parliament.uk/pa/ld199697/ldjudgmt/jd961121/smith01.htm", all)
-    # for a in all:
-    #     print(a)
-    #
-    # all = []
-    # ex.extract_case("https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-1.htm", all)
-    # for a in all:
-    #     print(a)
-    #
-    # print("\n\nNEW CASE\n\n")
-    #
-    # all = []
-    # ex.extract_case("https://publications.parliament.uk/pa/ld199697/ldjudgmt/jd961121/smith01.htm", all)
-    # for a in all:
-    #     print(a)
-    #
-    # print("\n\nNEW CASE\n\n")
+    # Testing extraction of links
+    raw_html = sc.simple_get("https://publications.parliament.uk/pa/ld/ldjudgmt.htm")
+    links = ex.extract_links(raw_html)
+    assert (len(links) == 835), error1
+    assert (links[-1] == "http://www.parliament.uk/site-information/copyright/"), error2
+
+    # Testing filtering of links
+    hol = ex.filter_holinks(links)
+    assert (len(hol) == 788), error1
+    assert (hol[0] == "https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/assom.htm"), error2
+    assert (hol[-1] == "https://publications.parliament.uk/pa/ld199697/ldjudgmt/jd961121/smith01.htm"), error2
+
+    assert ex.next_html("https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-1.htm") \
+     == "https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-2.htm", "ERROR, HTML does not increase."
+
+    print("Working")
+
+    # Testing case extraction
+    print("Visual inspection, check for duplicates:") #NOTE better test required
+    all = []
+    ex.extract_case("https://publications.parliament.uk/pa/ld199697/ldjudgmt/jd961121/smith01.htm", all, "test")
+    for a in all:
+        print(a)
 
     all = []
-    ex.extract_case("https://publications.parliament.uk/pa/ld200607/ldjudgmt/jd070207/beggs-1.htm", all, "07")
+    ex.extract_case("https://publications.parliament.uk/pa/ld200809/ldjudgmt/jd090617/attgen-1.htm", all, "test")
     for a in all:
-        print(a, "\nNEWLINE\n")
+        print(a)
+
+    print("\n\nNEW CASE\n\n")
+
+    all = []
+    ex.extract_case("https://publications.parliament.uk/pa/ld200607/ldjudgmt/jd071031/home-1.htm", all, "07")
+    for a in all:
+        print(a)
